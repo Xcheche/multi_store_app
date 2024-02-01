045e979960ddd3fcc97ec5af1d96135343a3cb05
xcheche <chekwubechukwu23@gmail.com>
2024-02-01 05:46:11 +0100

    search bar done

--- lib/main_screens/home.dart
+++ lib/main_screens/home.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+import '../minor_screens/search.dart';
+
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
@@ -17,7 +19,55 @@ class _HomeScreenState extends State<HomeScreen> {
         appBar: AppBar(
           elevation: 0,
           backgroundColor: Colors.white,
-          title: const CupertinoSearchTextField(),
+          title: InkWell(
+            onTap: () {
+              Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const SearchScreen()));
+            },
+            child: Container(
+              height: 35,
+              decoration: BoxDecoration(
+                border: Border.all(color: Colors.yellow, width: 1.4),
+                borderRadius: BorderRadius.circular(25),
+              ),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  const Row(
+                    children: [
+                      Padding(
+                        padding: EdgeInsets.symmetric(horizontal: 10),
+                        child: Icon(
+                          Icons.search,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Text(
+                        'What are you looking for?',
+                        style: TextStyle(color: Colors.grey, fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  Container(
+                    height: 32,
+                    width: 75,
+                    decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    child: const Center(
+                      child: Text(
+                        'Search',
+                        style: TextStyle(fontSize: 16, color: Colors.grey),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
           bottom: const TabBar(
             isScrollable: true,
             indicatorColor: Colors.yellow,
